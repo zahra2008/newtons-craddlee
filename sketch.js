eb2b0b0dfab23814aaa29b33a6b476cea37aafca
zahra2008 <86829083+zahra2008@users.noreply.github.com>
2021-10-08 15:25:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var roof;
+//Create multiple bobs, mulitple ropes varibale here
+
+
+function setup() {
+	createCanvas(800, 600);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var roof_options={
+		isStatic:true			
+	}
+
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
+    World.add(world,roof);
+
+	Engine.run(engine);
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("#99004d");
+
+  rect(roof.position.x,roof.position.y,230,20);
+
+  //call display() to show ropes here
+
+  
+  //create ellipse shape for multiple bobs here
+
+ 
+}
+
+//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
